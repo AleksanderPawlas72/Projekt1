97e2d50cb168e1b86fd6d4aecbb72da47ec71eca
Aleksander Pawlas <aleksander.pawlas@gmail.com>
2020-05-07 08:43:23 +0200

    Add project files.

---
+++ ProjektHello/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjektHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
