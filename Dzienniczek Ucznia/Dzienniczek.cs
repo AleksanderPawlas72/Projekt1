467118d1758b0aa7ca02d16d5ad855bd61445330
Aleksander Pawlas <aleksander.pawlas@gmail.com>
2020-05-08 14:50:21 +0200

    pierwszy dzeinniczek - utworzono obiekt dzienniczek, dodawanie ocen, obliczanie sredniej, wyciaganie ocen: max i min

---
+++ Dzienniczek Ucznia/Dzienniczek.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dzienniczek_Ucznia
+{
+    class Dzienniczek
+    {
+        //stan (zmienne - pola)
+        List<float> oceny = new List<float>();
+
+        //zachowania
+        public void DodawanieOcen(float ocena)
+        {
+            oceny.Add(ocena);
+        }
+
+        public float ObliczanieSredniej()
+        {
+            float suma = 0, srednia = 0;
+            foreach (var ocena in oceny)
+            {
+                suma += ocena;
+
+            }
+            srednia = suma / oceny.Count();
+            return srednia;
+        }
+        public float PobierzMaxOcene()
+        {
+            return oceny.Max();
+        }
+                public float PobierzMinOcene()
+        {
+            return oceny.Min();
+        }
+
+    }
+}
---
+++ Dzienniczek Ucznia/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Dzienniczek_Ucznia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dzienniczek dzienniczek = new Dzienniczek();
+            //dzienniczek.DodawanieOcen(5);
+            //dzienniczek.DodawanieOcen(8.5f);
+            //dzienniczek.DodawanieOcen(4.7f);
+
+            //float srednia = dzienniczek.ObliczanieSredniej();
+            //float max = dzienniczek.PobierzMaxOcene();
+            //float min = dzienniczek.PobierzMinOcene();
+
+            for (; ; )
+            {
+                Console.WriteLine("Podaj ocene z zakresu 1 do 10 ");
+
+                float ocena;
+                bool wynik = float.TryParse(Console.ReadLine(),out ocena);
+               
+                
+                if (ocena == 11)
+                {
+                    break;
+                }
+
+                if (wynik)
+                {
+                    if (ocena>0 && ocena<=10)
+                    {
+                        dzienniczek.DodawanieOcen(ocena);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Zła liczba, podaj z zakresu 1-10 ");
+                    }
+                }
+
+
+            }
+            Console.WriteLine("srednia: " + dzienniczek.ObliczanieSredniej());
+            Console.WriteLine("najwyzsza: " + dzienniczek.PobierzMaxOcene());
+            Console.WriteLine("najniższa: " + dzienniczek.PobierzMinOcene());
+
+            Console.ReadKey();
+
+            Dzienniczek dzienniczek2 = new Dzienniczek();
+            Dzienniczek dzienniczek3 = new Dzienniczek();
+            Dzienniczek dzienniczek4 = new Dzienniczek();
+            Dzienniczek dzienniczek5 = new Dzienniczek();
+            Dzienniczek dzienniczek6 = new Dzienniczek();
+
+        }
+    }
+
+
+}
